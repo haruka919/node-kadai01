c630a7e6cde695a42dd310dc106c6ecc1c25e54b
parukoo <hrk.0u0x@gmail.com>
2020-10-15 14:02:36 +0900

    データベース(mysql)と接続

--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,23 @@
-var router = require('express').Router();
+const router = require('express').Router();
+const mysql = require('mysql');
+
+const knex = require('knex')({
+  dialect: 'mysql',       // 使用するデータベースを指定
+  connection: {
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'node_db',
+    charset: 'utf8',
+  },
+});
+
+const BookShelf = require('bookshelf')(knex);
+
+const User = BookShelf.Model.extend({
+  hasTimestamps: true,
+  tableName: 'users',
+});
 
 router.get('/', (req, res) => {
   res.render('./register.ejs');
