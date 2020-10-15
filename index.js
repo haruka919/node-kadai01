82a75e4eb81684979fb5e027b7a58f90d5c0a41e
parukoo <hrk.0u0x@gmail.com>
2020-10-15 14:43:20 +0900

    バリデーションを追加

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 
+const validator = require('express-validator');
 const session = require('express-session');
-
+const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
 
 // テンプレートエンジンの指定
 app.set('view engine', 'ejs');
+
+// バリデーション
+app.use(validator());
+
+// POSTデータを取得する時に必要
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // セッションの設定
 const session_opt = {
@@ -18,6 +26,7 @@ const session_opt = {
   cookie: { maxAge: 60 * 60 * 1000 },
 };
 app.use(session(session_opt));
+
 
 app.use('/', indexRouter);
 app.use('/login', loginRouter);
--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const mysql = require('mysql');
 
 const knex = require('knex')({
   dialect: 'mysql',       // 使用するデータベースを指定
@@ -14,13 +13,50 @@ const knex = require('knex')({
 
 const BookShelf = require('bookshelf')(knex);
 
+// データベースにあるテーブルを扱うためのオブジェクトを作成
 const User = BookShelf.Model.extend({
   hasTimestamps: true,
   tableName: 'users',
 });
 
 router.get('/', (req, res) => {
-  res.render('./register.ejs');
+  let data = {
+    title: '会員登録',
+    form: {
+      name: '',
+      email: '',
+    },
+    content: '',
+  };
+  res.render('./register.ejs', data);
 });
+
+// ユーザー登録の送信処理
+router.post('/', (req, res, next) => {
+  req.check('name', 'NAME は必ず入力して下さい。').notEmpty();
+  req.check('email', 'EMAIL はメールアドレスを入力して下さい。').isEmail();
+  req.check('password', 'PASSWORD は7文字以上で入力して下さい。').isLength({ min: 7 });
+  req.assert('comfirm_password', '確認用の値と一致しません。').equals(req.body.password);
+  req.getValidationResult().then((result) => {
+    // バリデーションに引っかかったらエラーメッセージを表示
+    if (!result.isEmpty()) {
+      let content = '<ul class="error">';
+      let result_arr = result.array();　　// エラー内容が配列に返る
+      for (let n in result_arr) {
+        content += '<li>' + result_arr[n].msg + '</li>';
+      }
+      content += '</ul>';
+      let data = {
+        title: '会員登録',
+        content: content,
+        form: req.body,　// nameとemailは入力したものを返す
+      };
+      res.render('register', data);
+    } else {
+      console.log('OK')
+    }
+  })
+});
+
 
 module.exports = router;
