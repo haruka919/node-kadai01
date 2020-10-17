3d3473726a8209df02c4785c97f139d8e4746fc6
parukoo <hrk.0u0x@gmail.com>
2020-10-17 14:36:27 +0900

    controllerに切り分けた

---
+++ controllers/registerController.js
@@ -0,0 +1,51 @@
+module.exports = {
+  // ユーザー登録画面表示
+  show (req, res) {
+    const data = {
+      title: '会員登録',
+      form: {
+        name: '',
+        email: '',
+      },
+      content: '',
+    };
+    res.render('./register.ejs', data);
+  },
+  // ユーザー登録の送信処理
+  create (req, res) {
+    req.check('name', 'NAME は必ず入力して下さい。').notEmpty();
+    req.check('email', 'EMAIL はメールアドレスを入力して下さい。').isEmail();
+    req.check('password', 'PASSWORD は7文字以上で入力して下さい。').isLength({ min: 7 });
+    req.assert('comfirm_password', '確認用の値と一致しません。').equals(req.body.password);
+    req.getValidationResult().then((result) => {
+      // バリデーション失敗
+      if (!result.isEmpty()) {
+        let content = '<ul class="error">';
+        let result_arr = result.array();　　// エラー内容が配列に返る
+        for (let n in result_arr) {
+          content += '<li>' + result_arr[n].msg + '</li>';
+        }
+        content += '</ul>';
+        let data = {
+          title: '会員登録',
+          content: content,
+          form: req.body,　// nameとemailは入力したものを返す
+        };
+        res.render('register', data);
+      } else {
+        // バリデーション成功(ユーザー登録後、ホーム画面にリダイレクト)
+        new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        })
+        .save()
+        .then(() => {
+          // セッションにnameを保存
+          req.session.user = req.body.name;
+          res.redirect('/');
+        });
+      }
+    })
+  }
+}
--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const register = require('../controllers/registerController')
 
 const knex = require('knex')({
   dialect: 'mysql',       // 使用するデータベースを指定
@@ -19,55 +20,9 @@ const User = BookShelf.Model.extend({
   tableName: 'users',
 });
 
-router.get('/', (req, res) => {
-  let data = {
-    title: '会員登録',
-    form: {
-      name: '',
-      email: '',
-    },
-    content: '',
-  };
-  res.render('./register.ejs', data);
-});
+router.get('/', register.show);
 
 // ユーザー登録の送信処理
-router.post('/', (req, res, next) => {
-  req.check('name', 'NAME は必ず入力して下さい。').notEmpty();
-  req.check('email', 'EMAIL はメールアドレスを入力して下さい。').isEmail();
-  req.check('password', 'PASSWORD は7文字以上で入力して下さい。').isLength({ min: 7 });
-  req.assert('comfirm_password', '確認用の値と一致しません。').equals(req.body.password);
-  req.getValidationResult().then((result) => {
-    // バリデーション失敗
-    if (!result.isEmpty()) {
-      let content = '<ul class="error">';
-      let result_arr = result.array();　　// エラー内容が配列に返る
-      for (let n in result_arr) {
-        content += '<li>' + result_arr[n].msg + '</li>';
-      }
-      content += '</ul>';
-      let data = {
-        title: '会員登録',
-        content: content,
-        form: req.body,　// nameとemailは入力したものを返す
-      };
-      res.render('register', data);
-    } else {
-      // バリデーション成功(ユーザー登録後、ホーム画面にリダイレクト)
-      new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      })
-      .save()
-      .then(() => {
-        // セッションにnameを保存
-        req.session.user = req.body.name;
-        res.redirect('/');
-      });
-    }
-  })
-});
-
+router.post('/', register.create);
 
 module.exports = router;
